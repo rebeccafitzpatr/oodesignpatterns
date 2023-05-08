38d33725d240b3eea9fc4547e336fb6e3e83aa45
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-05-09 10:40:13 +1200

    created main class and espresso class

---
+++ Espresso.java
@@ -0,0 +1,11 @@
+public class Espresso {
+  
+  public void grindBeans(){
+    System.out.println("Use 9 grms of espresso roast coffee for a single espresso shot");
+  }
+
+  public void make() {
+    System.out.println("Put the portaffiller in the espresso machine, press the button");
+  }
+}
+
