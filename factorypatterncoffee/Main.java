9603488f09ba81ebf387181e6f0c9dba97c2974e
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-05-10 11:30:13 +1200

    use the factory method in the main class, take the user's input and the make the right cofee

--- factorypatterncoffee/Main.java
+++ factorypatterncoffee/Main.java
@@ -1,11 +1,22 @@
 package factorypatterncoffee;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    Espresso espresso = new Espresso();
-    espresso.grindBeans();
-    espresso.make();
+
+    //ask the user what kind of coffee they would like
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Hello welcome to our cafe!");
+    System.out.println("What kind of coffee would you like to order today??");
+    
+    String order = scan.nextLine();
+
+    Coffee coffee = CoffeeFactory.orderCoffee(order);
+    coffee.grindBeans();
+    coffee.make();
   }
 
   
