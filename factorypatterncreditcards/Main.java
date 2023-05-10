5860e797fd9473cc54b16604813cdb3ec036c7e1
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2023-05-10 14:43:38 +1200

    now the logic which decides which card type is correct is working

--- factorypatterncreditcards/Main.java
+++ factorypatterncreditcards/Main.java
@@ -1,14 +1,40 @@
 package factorypatterncreditcards;
 
+import java.util.Scanner;
+
 public class Main {
 
   public enum Income {SILVER, GOLD, PLATINUM}
 
   public static void main(String args[]) {
-    
+
+    Scanner scan = new Scanner(System.in);
+    int income = scan.nextInt();
+
+    //first convert the income so that it can be used by the factory method.
+    Income incomeClass = convertIncome(income);
+
+
+    CreditCard card = CreditCardFactory.createCard(incomeClass);  
 
   }
 
-  
-  
+  private static Income convertIncome(int income) {
+
+    if (income <= 99999) {
+      return Income.SILVER;
+
+    } else if ( income >= 100000 && income <= 199999) {
+      return Income.GOLD;
+
+    } else if (income >= 200000) {
+      return Income.PLATINUM;
+    } else {
+      return null;
+    }
+  }
 }
+
+  
+  
+
